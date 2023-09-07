d0c23ea421cae7e16f2dab5b6ec12f867be890d4
Arpit Bhalla <64216681+tec-wizard@users.noreply.github.com>
2023-09-07 12:17:57 +0530

    commented out methods that no longer work

--- src/commands.js
+++ src/commands.js
@@ -103,85 +103,85 @@ class Commands {
     return syntax;
   }
 
-  static async panda() {
-    const main = await axios.get("https://some-random-api.ml/animal/panda");
-    const data = await main.data;
+  // static async panda() {
+  //   const main = await axios.get("https://some-random-api.ml/animal/panda");
+  //   const data = await main.data;
 
-    if (!data) {
-      return console.log("couldnt access the API");
-    }
+  //   if (!data) {
+  //     return console.log("couldnt access the API");
+  //   }
 
-    let embed = {
-      image: {
-        url: `${data.image}`,
-      },
-      description: `${data.fact}`,
-    };
+  //   let embed = {
+  //     image: {
+  //       url: `${data.image}`,
+  //     },
+  //     description: `${data.fact}`,
+  //   };
 
-    let syntax = { embeds: [embed] };
+  //   let syntax = { embeds: [embed] };
 
-    return syntax;
-  }
+  //   return syntax;
+  // }
 
-  static async whale() {
-    const main = await axios.get("https://some-random-api.ml/animal/whale");
-    const data = await main.data;
+  // static async whale() {
+  //   const main = await axios.get("https://some-random-api.ml/animal/whale");
+  //   const data = await main.data;
 
-    if (!data) {
-      return console.log("couldnt access the API");
-    }
+  //   if (!data) {
+  //     return console.log("couldnt access the API");
+  //   }
 
-    let embed = {
-      image: {
-        url: `${data.image}`,
-      },
-      description: `${data.fact}`,
-    };
+  //   let embed = {
+  //     image: {
+  //       url: `${data.image}`,
+  //     },
+  //     description: `${data.fact}`,
+  //   };
 
-    let syntax = { embeds: [embed] };
+  //   let syntax = { embeds: [embed] };
 
-    return syntax;
-  }
+  //   return syntax;
+  // }
 
-  static async kangaroo() {
-    const main = await axios.get("https://some-random-api.ml/animal/kangaroo");
-    const data = await main.data;
+  // static async kangaroo() {
+  //   const main = await axios.get("https://some-random-api.ml/animal/kangaroo");
+  //   const data = await main.data;
 
-    if (!data) {
-      return console.log("couldnt access the API");
-    }
+  //   if (!data) {
+  //     return console.log("couldnt access the API");
+  //   }
 
-    let embed = {
-      image: {
-        url: `${data.image}`,
-      },
-      description: `${data.fact}`,
-    };
+  //   let embed = {
+  //     image: {
+  //       url: `${data.image}`,
+  //     },
+  //     description: `${data.fact}`,
+  //   };
 
-    let syntax = { embeds: [embed] };
+  //   let syntax = { embeds: [embed] };
 
-    return syntax;
-  }
+  //   return syntax;
+  // }
 
-  static async fox() {
-    const main = await axios.get("https://some-random-api.ml/animal/fox");
-    const data = await main.data;
+  // static async fox() {
+  //   const main = await axios.get("https://some-random-api.ml/animal/fox");
+  //   const data = await main.data;
 
-    if (!data) {
-      return console.log("couldnt access the API");
-    }
+  //   if (!data) {
+  //     return console.log("couldnt access the API");
+  //   }
 
-    let embed = {
-      image: {
-        url: `${data.image}`,
-      },
-      description: `${data.fact}`,
-    };
+  //   let embed = {
+  //     image: {
+  //       url: `${data.image}`,
+  //     },
+  //     description: `${data.fact}`,
+  //   };
 
-    let syntax = { embeds: [embed] };
+  //   let syntax = { embeds: [embed] };
 
-    return syntax;
-  }
+  //   return syntax;
+  // }
 
   static async joke() {
     const main = await axios.get("https://apis.duncte123.me/joke");
@@ -205,49 +205,49 @@ class Commands {
     return syntax;
   }
 
-  static async neko() {
-    const main = await axios.get("https://neko-love.xyz/api/v1/neko");
-    const data = await main.data;
+  // static async neko() {
+  //   const main = await axios.get("https://neko-love.xyz/api/v1/neko");
+  //   const data = await main.data;
 
-    if (data.code !== 200) {
-      return console.log("couldnt access the API");
-    }
+  //   if (data.code !== 200) {
+  //     return console.log("couldnt access the API");
+  //   }
 
-    let embed = {
-      color: 0xffc0cb,
-      image: {
-        url: `${data.url}`,
-      },
-    };
+  //   let embed = {
+  //     color: 0xffc0cb,
+  //     image: {
+  //       url: `${data.url}`,
+  //     },
+  //   };
 
-    let syntax = { embeds: [embed] };
+  //   let syntax = { embeds: [embed] };
 
-    return syntax;
-  }
+  //   return syntax;
+  // }
 
-  static async quote() {
-    const main = await axios.get(
-      "https://goquotes-api.herokuapp.com/api/v1/random?count=1"
-    );
-    const data = await main.data;
+  // static async quote() {
+  //   const main = await axios.get(
+  //     "https://goquotes-api.herokuapp.com/api/v1/random?count=1"
+  //   );
+  //   const data = await main.data;
 
-    if (!data) {
-      return console.log("couldnt access the API");
-    }
+  //   if (!data) {
+  //     return console.log("couldnt access the API");
+  //   }
 
-    let embed = {
-      fields: [
-        {
-          name: `${data.quotes[0].text}`,
-          value: `${data.quotes[0].author}`,
-        },
-      ],
-      color: 0xadd8e6,
-    };
+  //   let embed = {
+  //     fields: [
+  //       {
+  //         name: `${data.quotes[0].text}`,
+  //         value: `${data.quotes[0].author}`,
+  //       },
+  //     ],
+  //     color: 0xadd8e6,
+  //   };
 
-    let syntax = { embeds: [embed] };
-    return syntax;
-  }
+  //   let syntax = { embeds: [embed] };
+  //   return syntax;
+  // }
 
   static async advice() {
     const main = await axios.get("https://api.adviceslip.com/advice");
@@ -310,38 +310,38 @@ class Commands {
     return syntax;
   }
 
-  static async magic8ball(question) {
-    if (!question) {
-      return console.log("no question provided :/");
-    }
-    const main = await axios.get(
-      `https://8ball.delegator.com/magic/json/${encodeURIComponent(question)}`
-    );
-    const data = main.data;
+  // static async magic8ball(question) {
+  //   if (!question) {
+  //     return console.log("no question provided :/");
+  //   }
+  //   const main = await axios.get(
+  //     `https://8ball.delegator.com/magic/json/${encodeURIComponent(question)}`
+  //   );
+  //   const data = main.data;
 
-    if (!data) {
-      return console.log("couldnt access the API");
-    }
+  //   if (!data) {
+  //     return console.log("couldnt access the API");
+  //   }
 
-    let embed = {
-      fields: [
-        {
-          name: `your question`,
-          value: `${data.magic.question}`,
-        },
-        {
-          name: "my answer",
-          value: `${data.magic.answer}`,
-        },
-      ],
-      footer: {
-        text: `reply category : ${data.magic.type}`,
-      },
-    };
+  //   let embed = {
+  //     fields: [
+  //       {
+  //         name: `your question`,
+  //         value: `${data.magic.question}`,
+  //       },
+  //       {
+  //         name: "my answer",
+  //         value: `${data.magic.answer}`,
+  //       },
+  //     ],
+  //     footer: {
+  //       text: `reply category : ${data.magic.type}`,
+  //     },
+  //   };
 
-    let syntax = { embeds: [embed] };
+  //   let syntax = { embeds: [embed] };
 
-    return syntax;
-  }
+  //   return syntax;
+  // }
 }
 module.exports = Commands;
